621c9d2aecf444a8bb91301ced0c649d27751c05
SkY <akash15482066@gmail.com>
2025-08-20 18:43:20 +0530

    eslint

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,10 @@ const nextConfig: NextConfig = {
         hostname: "res.cloudinary.com",
       },
     ],
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
